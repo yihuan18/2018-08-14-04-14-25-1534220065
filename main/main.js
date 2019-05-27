d519308c6e1124ff67b91c057d4132874414ac84
yihuan18 <18200120142@163.com>
2019-05-27 20:24:50 +0800

    dependency intall

--- main/main.js
+++ main/main.js
@@ -1,4 +1,33 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(sequences) {
+    var prints = [  
+                    ['._.','|.|','|_|'],
+                    ['...','..|','..|'],
+                    ['._.','._|','|_.'],
+                    ['._.','._|','._|'],
+                    ['...','|_|','..|'],
+                    ['._.','|_.','._|'],
+                    ['._.','|_.','|_|'],
+                    ['._.','..|','..|'],
+                    ['._.','|_|','|_|'],
+                    ['._.','|_|','..|']
+                ];
+
+    var chars = sequences.split("");
+    var length = chars.length;
+    var nums = new Array(length);
+    for(var i = 0 ; i < length ; i++){
+        nums[i] = parseInt(chars[i]);
+    }
+    var ret = "";
+    for(var i = 0 ; i < 3 ; i++){
+        for(var j = 0 ; j < length ; j++){
+            ret += prints[nums[j]][i];
+            if(j != length - 1)
+                ret += " ";
+        }
+        ret += "\n";
+    }
+
+    return ret;
+    
 };
